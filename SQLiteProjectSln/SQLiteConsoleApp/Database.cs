4fa6b57b16670153e2f1aa78c23c139a28c6877a
Bob Bobga Nti <bobganti@yahoo.com>
2021-02-08 06:30:27 +0000

    Write and Read Funtionalitites

---
+++ SQLiteProjectSln/SQLiteConsoleApp/Database.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLiteConsoleApp
+{
+    class Database
+    {
+        public SQLiteConnection conn;
+
+        public Database()
+        {
+            conn = new SQLiteConnection("Data Source = database.sqlite3");
+
+            // Check if Database file already exists
+            if (!File.Exists("./database.sqlite3"))
+            {
+                SQLiteConnection.CreateFile("database.sqlite3");
+                Console.WriteLine("Database file is created");
+            }
+
+        }
+        
+        public void OpenConnection()
+        {
+            if(conn.State != System.Data.ConnectionState.Open)
+            {
+                conn.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (conn.State != System.Data.ConnectionState.Closed)
+            {
+                conn.Close();
+            }
+        }
+    }
+}
---
+++ SQLiteProjectSln/SQLiteConsoleApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLiteConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database db = new Database();
+            
+            // Insert into database
+            //string query = "INSERT INTO Albums ('Title', 'Artist') VALUES (@Title, @Artist)";
+            //SQLiteCommand command = new SQLiteCommand(query, db.conn);
+            //db.OpenConnection();
+            //command.Parameters.AddWithValue("@Title", "Brown Pizza");
+            //command.Parameters.AddWithValue("@Artist", "Nick Gp,ez");
+
+            //var rows = command.ExecuteNonQuery();
+            //db.CloseConnection();
+
+            //Console.WriteLine("Number of Insertions: {0}", rows);
+            //Console.ReadKey();
+
+
+            // Read from database
+            string query = "SELECT * FROM Albums";
+            SQLiteCommand command = new SQLiteCommand(query, db.conn);
+            db.OpenConnection();
+
+            SQLiteDataReader result = command.ExecuteReader();
+            if (result.HasRows)
+            {
+                while (result.Read())
+                {
+                    Console.WriteLine("Album: {0} Artist: {1}", result[1], result[2]);
+                }
+            }
+            db.CloseConnection();
+
+            Console.ReadKey();
+        }
+    }
+}
